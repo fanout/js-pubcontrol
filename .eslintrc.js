ea9e736b3b571f375cc73bf90cc7687c407d03b0
Benjamin Goering <ben@bengo.co>
2018-12-17 12:57:41 -0800

    prettier .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-	"extends": ["plugin:prettier/recommended"],
-	"parserOptions": {
-		"ecmaFeatures": {
-      "experimentalObjectRestSpread": true
+  extends: ["plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true
     },
-		"ecmaVersion": 2017,
-		"sourceType": "module"
-	},
+    ecmaVersion: 2017,
+    sourceType: "module"
+  }
 };
