e4306c1e8cea4f4f9647099b6fe162c31cb31abd
Katsuyuki Ohmuro <harmony7@pex2.jp>
2019-12-23 14:52:38 +0900

    reexport main export for commonjs

---
+++ commonjs-index.js
@@ -0,0 +1,8 @@
+var esModule = require('./main.js');
+
+module.exports = esModule.default;
+module.exports.Auth = esModule.Auth;
+module.exports.Format = esModule.Format;
+module.exports.Item = esModule.Item;
+module.exports.PubControlClient = esModule.PubControlClient;
+module.exports.base64ToBuffer = esModule.base64ToBuffer;
